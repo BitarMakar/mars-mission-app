978e61f10431e7e032d305dad610c07f136f7259
BitarMakar <t8210220@aueb.gr>
2025-04-30 00:39:35 +0300

    separated server run from main app into index

--- app.js
+++ app.js
@@ -13,29 +13,29 @@ app.post("/api/products", async (req, res) => {
   const { name, price, quantity } = req.body;
 
   if (isUndefinedOrNullOrEmpty(name) || isUndefinedOrNull(price, quantity))
-    res.status(400).json({ error: "Invalid body provided." });
+    return res.status(400).json({ error: "Invalid body provided." });
 
   try {
     const product = await db.product.create({
       data: { name, price, quantity },
     });
-    res.status(201).json(product);
+    return res.status(201).json(product);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "A server error has occured" });
+    return res.status(500).json({ error: "A server error has occured" });
   }
 });
 
 app.get("/api/products", async (req, res) => {
   const products = await db.product.findMany();
-  res.json(products || [])  ;
+  return res.json(products || []);
 });
 
 app.get("/api/products/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   const product = await db.product.findUnique({ where: { id } });
   if (!product) return res.status(404).json({ error: "Product not found" });
-  res.json(product);
+  return res.json(product);
 });
 
 app.patch("/api/products/:id", async (req, res) => {
@@ -47,10 +47,10 @@ app.patch("/api/products/:id", async (req, res) => {
       where: { id },
       data: { name, price, quantity },
     });
-    res.json(product);
+    return res.json(product);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "A server error has occured" });
+    return res.status(500).json({ error: "A server error has occured" });
   }
 });
 
@@ -58,15 +58,11 @@ app.delete("/api/products/:id", async (req, res) => {
   const id = parseInt(req.params.id);
   try {
     await db.product.delete({ where: { id } });
-    res.status(204).send();
+    return res.status(204).send();
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "A server error has occured" });
+    return res.status(500).json({ error: "A server error has occured" });
   }
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+module.exports = app;
